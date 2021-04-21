faece10c5d92c8c2877a057d8c0050eccf0feb5a
瑋芝 張 <juice121457@gmail.com>
2021-04-21 16:16:56 +0800

    DroneMedel變更

--- Models/DroneMedel.cs
+++ Models/DroneMedel.cs
@@ -7,13 +7,18 @@ namespace Yubay_Drone_team.Models
 {
     public class DroneMedel
     {
-        public int Number { get; set; }
+        public int Sid { get; set; }
 
-        public string Manufacturer { get; set; }
+        public string Drone_ID { get; set; }
 
-        public string Weight { get; set; }
+        public string WeightLoad { get; set; }
         public string Status { get; set; }
-        public string Deactive { get; set; }
-        public string Person { get; set; }
+        public string StopReason { get; set; }
+        public string Operator { get; set; }
+        public string Updater { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool IsDelete { get; set; }
+        public string Delete { get; set; }
+        public DateTime DeleteDate { get; set; }
     }
 }
