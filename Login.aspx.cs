af42c98e52587f2c09b1a356dea84ec62b928ff3
Kos8943 <a26095998@gmail.com>
2021-04-21 11:51:51 +0800

    登入功能部分完成(可登入)

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Yubay_Drone_team.Helpers;
 
 namespace Yubay_Drone_team
 {
@@ -12,6 +13,35 @@ namespace Yubay_Drone_team
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string Account = this.TextAccount.Text;
+
+            string Password = this.TextPassword.Text;
+
+            if( string.IsNullOrEmpty(Account) || string.IsNullOrEmpty(Password))
+            {
+                this.ltErrorMsg.Text = "請輸入帳號密碼";
+
+                this.ltErrorMsg.Visible = true;
+
+                return;
+            }
+
+            if(LoginHelper.TryLogin(Account, Password))
+            {
+                Response.Redirect("User_Account.aspx");
+            }
+            else
+            {
+                this.ltErrorMsg.Text = "帳號或密碼錯誤";
+
+                this.ltErrorMsg.Visible = true;
+
+                return;
+            }
         }
     }
 }
---
+++ Models/AccountModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yubay_Drone_team.Models
+{
+    public class AccountModel
+    {
+        public int Sid { get; set; }
+
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public bool SuperAccount { get; set; }
+
+        public string UserName { get; set; }
+
+        public int AccountLevel { get; set; }
+
+        public string Updater { get; set; }
+
+        public int UpdateDate { get; set; }
+
+        public int IsDelete { get; set; }
+
+        public string Deleter { get; set; }
+
+        public int DeleteDate { get; set; }
+        //Sid 
+        //Account 
+        //Password    
+        //SuperAccount   
+        //UserName 
+        //AccountLevel    
+        //Updater 
+        //UpdateDate  
+        //IsDelete 
+        //Deleter 
+        //DeleteDate  
+    }
+}
