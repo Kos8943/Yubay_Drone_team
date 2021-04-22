eb32bf7df294a2d4914911f85a4ac6933583f99d
瑋芝 張 <juice121457@gmail.com>
2021-04-22 14:22:25 +0800

    提取內容

--- Managers/ConnectionDB.cs
+++ Managers/ConnectionDB.cs
@@ -4,50 +4,49 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using Yubay_Drone_team.Models;
+using Yubay_Drone_team.Helpers;
 
 namespace Yubay_Drone_team.Managers
 {
-    public class ConnectionDB
+    public class ConnectionDB:CreateHelper
     {
-        public void Drone_Detail_Create(string Drone_ID, string Manufacturer, string WeightLoad, string Status, string StopReason, string Operator)
+        public void Drone_Detail_Create(DroneMedel Model)
         {
-            //建立資料庫字串變數
-            string connectionString = "Data Source=localhost\\SQLExpress;Initial Catalog=Yubay_Drone; Integrated Security=true";
+            
             //使用的SQL語法
             string queryString = $@" INSERT INTO Drone_Detail (Drone_ID, Manufacturer, WeightLoad, Status, StopReason, Operator)
                                         VALUES (@Drone_ID, @Manufacturer, @WeightLoad, @Status, @StopReason, @Operator);";
 
             //建立連線
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                //轉換成SQL可讀懂的語法
-                SqlCommand command = new SqlCommand(queryString, connection);
-                command.Parameters.AddWithValue("@Drone_ID", Drone_ID);
-                command.Parameters.AddWithValue("@Manufacturer", Manufacturer);
-                command.Parameters.AddWithValue("@WeightLoad", WeightLoad);
-                command.Parameters.AddWithValue("@Status", Status);
-                command.Parameters.AddWithValue("@StopReason", StopReason);
-                command.Parameters.AddWithValue("@Operator", Operator);
 
-                try
+
+            //轉換成SQL可讀懂的語法
+            //SqlCommand command = new SqlCommand(queryString, connection);
+            //command.Parameters.AddWithValue("@Drone_ID", Drone_ID);
+            //command.Parameters.AddWithValue("@Manufacturer", Manufacturer);
+            //command.Parameters.AddWithValue("@WeightLoad", WeightLoad);
+            //command.Parameters.AddWithValue("@Status", Status);
+            //command.Parameters.AddWithValue("@StopReason", StopReason);
+            //command.Parameters.AddWithValue("@Operator", Operator);
+            List<SqlParameter> parameters = new List<SqlParameter>()
+
                 {
-                    //開始連線
-                    connection.Open();
-                   
-                    //關閉資料庫連線
-                    connection.Close();
-                    //回傳dt
-                    
+                   new SqlParameter("@Drone_ID", Model.Drone_ID),
+                   new SqlParameter("@Manufacturer",Model.Manufacturer),
+                   new SqlParameter("@WeightLoad",Model.WeightLoad),
+                   new SqlParameter("@Status",Model.Status),
+                   new SqlParameter("@StopReason", Model.StopReason),
+                   new SqlParameter("@Operator", Model.Operator)
 
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    
+                };
 
-                }
+            this.ExecuteNonQuery(queryString, parameters);
+            
 
-            }
+
+             
+            
         }
     }
 }
